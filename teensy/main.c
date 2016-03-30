c37bcb771826891c8fa92b3c230894c9d403f20f
Mike <otakdequintar@yahoo.com>
2016-03-30 09:54:10 -0400

    Initial Commit

---
+++ teensy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  teensy
+//
+//  Created by Mike Lyons on 3/30/16.
+//  Copyright © 2016 Mike Lyons. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
